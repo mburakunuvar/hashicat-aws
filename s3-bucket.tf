9eeb2217df92fcdab372e83d706d4579b9e0e348
mburakunuvar <mburakunuvar@gmail.com>
2023-10-04 14:55:10 +0000

    Added s3 bucket module 3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/buraku-aws/s3-bucket/aws"
+  version = "2.8.0"
 }
